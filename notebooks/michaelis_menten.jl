fc321e4a2fddaccd7b45d26da5e88ef36b2486ba
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2023-11-10 16:40:10 -0300

    arregle sin val en mm

--- notebooks/michaelis_menten.jl
+++ notebooks/michaelis_menten.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.32
+# v0.19.28
 
 using Markdown
 using InteractiveUtils
@@ -26,16 +26,17 @@ md"""
 end
 
 # ╔═╡ c1c2ee13-e1c4-46e4-9094-2a0f277cb9b4
-begin
-	@suppress begin
-		using Plots, PlutoUI
-		gr()
-	end;
+@suppress begin
+	using Plots, PlutoUI, AbstractPlutoDingetjes
+	gr()  
 end;
 
 # ╔═╡ d75b9927-8f62-4a80-8465-c93f6a72e25e
-function michaelis_menten(vmax, km, s)
-    return (vmax * s) / (km + s)
+begin
+	function michaelis_menten(vmax, km, s)
+    	return (vmax * s) / (km + s)
+	end;
+	limpito(entered_val) = entered_val isa NamedTuple  ? 0 : entered_val
 end;
 
 # ╔═╡ f953b867-2661-49ee-aec8-674a1ae07181
@@ -69,7 +70,7 @@ md"""
 """
 
 # ╔═╡ 300756a9-21cd-42cd-a833-0a725b5b7094
-plot(1:s_range_max, s -> michaelis_menten(v, km, s), label="Curva de MM", xlabel="[S]", ylabel="V");hline!([vmax], label="Vmax");plot!(size=(750,750))
+plot(1:s_range_max, s -> michaelis_menten(v, km, s), label="Curva de MM", xlabel="[S]", ylabel="V");hline!([limpito(vmax)], label="Vmax");plot!(size=(750,750))
 
 # ╔═╡ 2c0eb34a-09cb-49d3-b22f-dec433a0d948
 md"""
@@ -93,11 +94,13 @@ Enviar correo:
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
+AbstractPlutoDingetjes = "6e696c72-6542-2067-7265-42206c756150"
 Plots = "91a5bcdd-55d7-5caf-9e0b-520d859cae80"
 PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
 Suppressor = "fd094767-a336-5f1f-9728-57cf17d0bbfb"
 
 [compat]
+AbstractPlutoDingetjes = "~1.2.0"
 Plots = "~1.39.0"
 PlutoUI = "~0.7.53"
 Suppressor = "~0.2.6"
@@ -107,9 +110,9 @@ Suppressor = "~0.2.6"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.9.2"
+julia_version = "1.9.3"
 manifest_format = "2.0"
-project_hash = "8e9de5e93f27866f918ebf4bc2ca77dd4765da16"
+project_hash = "27eae31427d4eafc9a0303281c6a2ec99600bea6"
 
 [[deps.AbstractPlutoDingetjes]]
 deps = ["Pkg"]
@@ -938,9 +941,9 @@ version = "1.1.34+0"
 
 [[deps.XZ_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl"]
-git-tree-sha1 = "522b8414d40c4cbbab8dee346ac3a09f9768f25d"
+git-tree-sha1 = "cf2c7de82431ca6f39250d2fc4aacd0daa1675c0"
 uuid = "ffd25f8a-64ca-5728-b0f7-c24cf3aae800"
-version = "5.4.5+0"
+version = "5.4.4+0"
 
 [[deps.Xorg_libICE_jll]]
 deps = ["Libdl", "Pkg"]
@@ -1204,8 +1207,8 @@ version = "1.4.1+1"
 # ╟─f953b867-2661-49ee-aec8-674a1ae07181
 # ╟─b7983948-71ca-4a23-8a87-d10e4d20561e
 # ╟─3a2fabfc-4f94-4f05-9d15-3f3b6e635a9e
-# ╟─da2255b1-0bcc-4813-bbf7-0d29055aadfd
-# ╟─300756a9-21cd-42cd-a833-0a725b5b7094
+# ╠═da2255b1-0bcc-4813-bbf7-0d29055aadfd
+# ╠═300756a9-21cd-42cd-a833-0a725b5b7094
 # ╟─2c0eb34a-09cb-49d3-b22f-dec433a0d948
 # ╟─9969812a-5c43-43f3-89ac-d41dd1fa3b97
 # ╟─dbad60f3-e143-47de-8369-76c6afc8c76f
